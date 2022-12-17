781164c9d8921268af800f1838bd78e5c74c831f
Muxiddin86 <imamberdievmuxiddin@gmail.com>
2022-12-17 19:52:46 +0500

    Salom

---
+++ main.py
@@ -0,0 +1,57 @@
+a = 5
+b = 4
+c = a + b
+print(c)
+
+a = 7
+b = 3
+c = a - b
+print(c)
+
+a = 6
+b = 8
+c = a * b
+print(c)
+
+a = 12
+b = 3
+c = a / b
+print(c)
+
+a = 7
+b = 3
+c = 5
+S = a + b + c
+print(S)
+
+a = 24
+b = 3
+c = 5
+k = (a - b) + c
+print(k)
+
+a = 5
+b = 4
+c = 8
+k = (2*a / b) * c
+print(k)
+
+a = 5
+b = 4
+c = 8
+k = (a * b) / c
+print(k)
+
+a = 8
+b = 3
+c = 2
+d = 4
+k = a + b - c * d
+print(k)
+
+a = 5
+b = 4
+c = 2
+d = 8
+k = 2*(a * b - c * d)
+print(k)
